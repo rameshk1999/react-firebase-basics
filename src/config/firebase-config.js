35c1b169896cfb27cb4ac0ab1b338cf4b7c2b50c
rameshk1999 <kasularamesh7337@gmail.com>
2021-11-15 10:55:44 +0530

    updated

---
+++ src/config/firebase-config.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-khxx94X0_dXxnuzLdsMV9-AAVGZpkF8",
+  authDomain: "react-firebase-1885a.firebaseapp.com",
+  projectId: "react-firebase-1885a",
+  storageBucket: "react-firebase-1885a.appspot.com",
+  messagingSenderId: "202931593138",
+  appId: "1:202931593138:web:a17d960506245713808eab",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be in the authorized domains list in the Firebase Console.
+  url: "https://www.example.com/finishSignUp?cartId=1234",
+  // This must be true.
+  handleCodeInApp: true,
+  iOS: {
+    bundleId: "com.example.ios",
+  },
+  android: {
+    packageName: "com.example.android",
+    installApp: true,
+    minimumVersion: "12",
+  },
+  dynamicLinkDomain: "example.page.link",
+};
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/system";
+import React from "react";
+import Header from "../components/layouts/Header";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header />
+      <Box>hello world</Box>
+    </div>
+  );
+}
